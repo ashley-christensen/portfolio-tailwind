63df31203fff98935de1e3593fb5362da58bfe52
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-09-26 18:15:47 -0700

    home page styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,14 +7,14 @@ const Home = () => {
 		<div name='home' className='w-full h-screen bg-gradient-to-b from-black via-black to-gray-800'>
 			<div className=' max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4'>
 				<div className='flex flex-col justify-center h-full'>
-					<h2 className='text-4xl sm:text-7xl font-bold textLink'>Welcome. </h2>
+					<h2 className='text-4xl sm:text-7xl font-bold textLink'>Thank you for visiting </h2>
 					<p className='textLink py-4 max-w-md'>
-						Thank you for checking out my web site. I'm a full stack developer with two years experience in
-						software development. I'm passionate at developing apps, improving programs and debugging code.
-						Seeking a position in tech as a software developer, customer service engineer or technical
-						account support rep. With work experience in sales and education, I'm adept at presentations
-						that convey technical value to stakeholders at all levels. I'm currently learning Typescript.
-						Java is next. I enjoy learning and I look forward to being part of a great team!
+						I'm a full stack developer with two years experience in software development. I'm passionate at
+						developing apps, improving programs and debugging code. Seeking a position in tech as a software
+						developer, customer service engineer or technical account support rep. With work experience in
+						sales and education, I'm adept at presentations that convey technical value to stakeholders at
+						all levels. I'm currently learning Typescript. Java is next. I enjoy learning and I look forward
+						to being part of a great team!
 					</p>
 					<div>
 						<Link
